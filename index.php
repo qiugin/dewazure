9f5bc177dd63b350b077b9ccd55e6a1ae85cc3ce
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 01:13:39 +0700

    v3

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "$conn,SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
