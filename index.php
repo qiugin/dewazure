44d8e46e4c12d2ca763a7606ea88ecfd006f3081
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 00:06:20 +0700

    v

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+    $host = "dewserver.database.windows.net";
+    $user = "dewadmin";
+    $pass = "Dewohat97";
+    $db = "yemDB";
 
-echo "wadudu";
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
