5c99246e6bb335151e670b805a4cb2192a93938e
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 00:19:52 +0700

    v

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,26 @@
+
+
+
 <?php 
 
-$koneksi = mysqli_connect("dewserver.database.windows.net","dewadmin","Dewohat97","yemDB");
+// $koneksi = mysqli_connect("dewserver.database.windows.net","dewadmin","Dewohat97","yemDB");
 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
+// if (mysqli_connect_errno()){
+// 	echo "Koneksi database gagal : " . mysqli_connect_error();
+// }
 
 ?>
+
+
+<?php
+    $host = "dewserver.database.windows.net";
+    $user = "dewadmin";
+    $pass = "Dewohat97";
+    $db = "yemDB";
+
+    try {
+        $koneksi = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $koneksi->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
