11dc954f95be02715de7b6cf0fad1c57a7eeef60
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 02:30:46 +0700

    v3

--- tambah.php
+++ tambah.php
@@ -11,32 +11,34 @@ if( empty($_SESSION['email']) ){
 
 
 <html>
- <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/style.css">
+     
+     <Title>Registration Form</Title>
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <a href="mhs.php">List Mahasiswa</a>
- <form method="post" action="" enctype="multipart/form-data" >
-       Name  <input type="text" name="nama_dew" id="nama_dew"/></br></br>
-       nim <input type="text" name="nim_dew" id="nim_dew"/></br></br>
-       prodi <input type="text" name="prodi_dew" id="prodi_dew"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
- </form>
+
+ <div class="center">
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <a href="mhs.php"><button type="button" class="btn btn-info">Daftar Mahasiswa</button></a>
+    
+    <form class="form" method="post" action="" enctype="multipart/form-data" >
+          Name  <input type="text" name="nama_dew" id="nama_dew"/></br></br>
+          nim <input type="text" name="nim_dew" id="nim_dew"/></br></br>
+          prodi <input type="text" name="prodi_dew" id="prodi_dew"/></br></br>
+          <input type="submit" name="submit" value="Tambah" />
+          <button type="button" name="submit" class="btn btn-success">Success</button>
+    </form>
+</div>
+
+
  <?php
     $host = "dewserver.database.windows.net";
     $user = "dewadmin";
