ab490887d1e32b84130c6e4ba71a5c72892edd72
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 01:28:52 +0700

    v3

--- profil.php
+++ profil.php
@@ -9,13 +9,72 @@ if( empty($_SESSION['email']) ){
 
 ?>
 
-<!DOCTYPE html>
-<head>
-    <title>Profil</title>
-</head>
-<body>
-    <!-- Menampilkan isi session email -->
-    <h3> Hallo Selamat Datang <?php echo $_SESSION['email']; ?> </h3>
+    
+
+<html>
+ <head>
+ <Title>Registration Form</Title>
+ <style type="text/css">
+ 	body { background-color: #fff; border-top: solid 10px #000;
+ 	    color: #333; font-size: .85em; margin: 20; padding: 20;
+ 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+ 	}
+ 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+ 	h1 { font-size: 2em; }
+ 	h2 { font-size: 1.75em; }
+ 	h3 { font-size: 1.2em; }
+ 	table { margin-top: 0.75em; }
+ 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+ </style>
+ </head>
+ <body>
+<!-- Menampilkan isi session email -->
+<h3> Hallo Selamat Datang <?php echo $_SESSION['email']; ?> </h3>
     <a href="logout.php">Keluar</a>
-</body>
-</html>
+ <?php
+    $host = "dewserver.database.windows.net";
+    $user = "dewadmin";
+    $pass = "Dewohat97";
+    $db = "yemDB";
+
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
+
+        try {
+            $sql_select = "SELECT * FROM mhs_dew";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['nama_dew']."</td>";
+                    echo "<td>".$registrant['nim_dew']."</td>";
+                    echo "<td>".$registrant['prodi_dew']."</td>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+    
+
+ ?>
+
+
+
+
+ </body>
+ </html>
