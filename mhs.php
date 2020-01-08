064b1d7f2ace25526359628eec84a196173603db
dewaqintoro <dewaqintoro@gmail.com>
2020-01-09 03:29:33 +0700

    v4

--- mhs.php
+++ mhs.php
@@ -22,10 +22,19 @@ if( empty($_SESSION['username']) ){
      <title>SIA</title>
  </head>
  <body>
+
+ <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">DEW</a>
+  <form class="form-inline">
+    <a href="logout.php"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button></a>
+    
+  </form>
+</nav>
+
 <div class="center2">
 <!-- Menampilkan isi session username -->
 <h3> Hallo Selamat Datang <?php echo $_SESSION['username']; ?> </h3>
-    <a href="logout.php">Keluar</a>
+    
  <?php
     $host = "dewserver.database.windows.net";
     $user = "dewadmin";
@@ -44,9 +53,8 @@ if( empty($_SESSION['username']) ){
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
                 echo '<a href="tambah.php">Tambah Data</a>';
-                
+                echo '<a href="tambah.php"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tambah Data</button></a>';
                 echo "<table>";
                 echo "<tr><th>Name</th>";
                 echo "<th>NIM</th>";
@@ -69,7 +77,9 @@ if( empty($_SESSION['username']) ){
  ?>
 
 
-
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </div>
  </body>
  </html>
